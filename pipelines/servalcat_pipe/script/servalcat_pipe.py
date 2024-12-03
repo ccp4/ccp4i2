5decc922fa7d47c2be9e9e9d27f20579b49eeb22
Martin Maly <martin.maly.mm@email.cz>
2024-12-03 22:12:10 +0000

    delete unittest

--- pipelines/servalcat_pipe/script/servalcat_pipe.py
+++ pipelines/servalcat_pipe/script/servalcat_pipe.py
@@ -1073,32 +1073,3 @@ def exportJobFileMenu(jobId=None):
             ['2FoFc_as_map', '2FoFc as map', 'application/CCP4-map'],
             ['FoFc_as_map', 'FoFc as map', 'application/CCP4-map'],
             ]
-
-#============================================================================================
-import unittest
-class testRefmac(unittest.TestCase):
-
-    def test1(self):
-        # Test creation of log file using ../test_data/test1.params.xml input
-        from core.CCP4Utils import getCCP4I2Dir
-        import os
-        workDirectory = CCP4Utils.getTestTmpDir()
-        logFile = os.path.join(workDirectory,'prosmart_refmac_test1.log')
-        # Delete any existing log file
-        if os.path.exists(logFile): os.remove(logFile)
-        self.wrapper = prosmart_refmac(name='prosmart_refmac_test1',workDirectory=workDirectory)
-        self.wrapper.container.loadDataFromXml(os.path.join(getCCP4I2Dir(),'wrappers','prosmart_refmac','test_data','test1.params.xml'))
-        self.wrapper.setWaitForFinished(1000000)
-        pid = self.wrapper.process()
-        self.wrapper.setWaitForFinished(-1)
-        if len(self.wrapper.errorReport)>0: print(self.wrapper.errorReport.report())
-#self.assertTrue(os.path.exists(logFile),'No log file found')
-
-
-def TESTSUITE():
-    suite = unittest.TestLoader().loadTestsFromTestCase(testRefmac)
-    return suite
-
-def testModule():
-    suite = TESTSUITE()
-    unittest.TextTestRunner(verbosity=2).run(suite)
