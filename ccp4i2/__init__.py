071128f413c30afb28b675517fdd7201821b9650
Paul Bond <git@paulsbond.co.uk>
2025-03-04 16:47:43 +0000

    Added __init__.py files

--- ccp4i2/__init__.py
+++ ccp4i2/__init__.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
 __version_date__ = "04 Mar 2025"
 __version_info__ = (2, 2, 4)
 __version__ = ".".join(map(str, __version_info__))
+
+I2_TOP = Path(__file__).resolve().parent
