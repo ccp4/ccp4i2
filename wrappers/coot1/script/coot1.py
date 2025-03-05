77bfd2e4813a70478f5b532e1c32d9ec08167722
Paul Bond <git@paulsbond.co.uk>
2025-03-05 14:08:16 +0000

    Coot 1 updates

--- wrappers/coot1/script/coot1.py
+++ wrappers/coot1/script/coot1.py
@@ -1,40 +1,49 @@
-import os
+from os import environ
+from pathlib import Path
 from core.CCP4PluginScript import CPluginScript
-from core.CCP4Modules import PROJECTSMANAGER
 
 
 class coot1(CPluginScript):
-    TASKMODULE = 'model_building'
-    TASKTITLE = 'Coot 1'
-    TASKNAME = 'coot1'
-    TASKCOMMAND = '/home/paul/programs/ccp4-9/coot_py3/bin/coot'
-    TASKVERSION = 0.0
+    TASKNAME = "coot1"
+    TASKMODULE = "model_building"
+    TASKTITLE = "Coot 1"
+    TASKCOMMAND = str(Path(environ["CCP4"], "coot_py3", "bin", "coot"))
+    TASKVERSION = 0.1
+    DESCRIPTION = "Interactive model building with Coot 1)"
     ASYNCHRONOUS = True
     TIMEOUT_PERIOD = 9999999.9
-    MAINTAINER = 'martin.noble@newcastle.ac.uk'
+    MAINTAINER = "paul.bond@york.ac.uk"
+    WHATNEXT = ["prosmart_refmac", "coot_rebuild", "modelcraft"]
 
     ERROR_CODES = {}
 
     def makeCommandAndScript(self):
+        inputData = self.container.inputData
+
+        if inputData.XYZIN_LIST.isSet():
+            for path in inputData.XYZIN_LIST:
+                self.appendCommandLine(["--coords", path])
+                # script.append(f"coot.read_coordinates('{path}')")
+
+        if inputData.DICT.isSet():
+            self.appendCommandLine(["--dictionary", inputData.DICT])
+
         script = ["import coot"]
-        if self.container.inputData.XYZIN_LIST.isSet():
-            for path in self.container.inputData.XYZIN_LIST:
-                script.append(f"coot.read_coordinates('{path}')")
-        if self.container.inputData.FPHIIN_LIST.isSet():
-            for path in self.container.inputData.FPHIIN_LIST:
+        if inputData.FPHIIN_LIST.isSet():
+            for path in inputData.FPHIIN_LIST:
                 script.append(f"coot.read_mtz('{path}', 'F', 'PHI', '', False, False)")
-        if self.container.inputData.DELFPHIIN_LIST.isSet():
-            for path in self.container.inputData.DELFPHIIN_LIST:
+        if inputData.DELFPHIIN_LIST.isSet():
+            for path in inputData.DELFPHIIN_LIST:
                 script.append(f"coot.read_mtz('{path}', 'F', 'PHI', '', False, True)")
-        if self.container.inputData.DELFPHIINANOM_LIST.isSet():
-            for path in self.container.inputData.DELFPHIINANOM_LIST:
-                script.append(f"imap = coot.read_mtz('{path}', 'F', 'PHI', '', False, True)")
+        if inputData.DELFPHIINANOM_LIST.isSet():
+            for path in inputData.DELFPHIINANOM_LIST:
+                script.append(
+                    f"imap = coot.read_mtz('{path}', 'F', 'PHI', '', False, True)"
+                )
                 script.append("coot.set_map_colour(imap, 0.75, 0.9, 0.75)")
-        jobDirectory = PROJECTSMANAGER().db().jobDirectory(self.jobId)
-        scriptPath = os.path.join(jobDirectory, "coot_script.py")
-        with open(scriptPath, "w") as stream:
+        scriptPath = Path(self.getWorkDirectory(), "coot_script.py")
+        with scriptPath.open("w", encoding="utf-8") as stream:
             stream.write("\n".join(script))
         self.appendCommandLine(["--script", scriptPath])
-        if self.container.inputData.COOTSCRIPTFILE.isSet():
-            self.appendCommandLine(["--script", self.container.inputData.COOTSCRIPTFILE])
+
         return CPluginScript.SUCCEEDED
