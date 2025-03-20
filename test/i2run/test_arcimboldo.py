5ac6420f563bd7c6f780b34610eccd88287e675b
Paul Bond <git@paulsbond.co.uk>
2025-03-20 16:35:44 +0000

    Arcimboldo test

---
+++ test/i2run/test_arcimboldo.py
@@ -0,0 +1,11 @@
+from .utils import demoData, i2run
+
+
+def test_arcimboldo():
+    args = ["arcimboldo"]
+    args += ["--F_SIGF", demoData("gamma", "merged_intensities_Xe.mtz")]
+    args += ["--ARCIMBOLDO_OPTIONS", "LITE"]
+    args += ["--MOLECULAR_WEIGHT", "15000"]
+    args += ["--N_FRAGMENTS", "1"]
+    with i2run(args) as directory:
+        assert directory.exists()
