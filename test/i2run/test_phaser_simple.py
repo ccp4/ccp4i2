3f18b379d214eca08df1442e4df3638ece23d208
Paul Bond <git@paulsbond.co.uk>
2025-03-25 11:47:55 +0000

    Test phaser simple

---
+++ test/i2run/test_phaser_simple.py
@@ -0,0 +1,22 @@
+import gemmi
+from .utils import demoData, i2run
+
+
+def test_gamma():
+    args = ["phaser_simple"]
+    args += ["--F_SIGF", demoData("gamma", "merged_intensities_Xe.mtz")]
+    args += ["--XYZIN", demoData("gamma", "gamma_model.pdb")]
+    with i2run(args) as directory:
+        for name in ["PHASER.1", "XYZOUT_REFMAC", "XYZOUT_SHEETBEND"]:
+            gemmi.read_pdb(str(directory / f"{name}.pdb"))
+        for name in ["DIFMAPOUT_1", "MAPOUT_1", "PHASEOUT_1"]:
+            gemmi.read_mtz_file(str(directory / f"{name}.mtz"))
+
+
+def test_no_solution():
+    args = ["phaser_simple"]
+    args += ["--F_SIGF", demoData("gamma", "merged_intensities_Xe.mtz")]
+    args += ["--XYZIN", demoData("rnase", "rnase_model.pdb")]
+    args += ["--RESOLUTION_HIGH", "5.0"]
+    with i2run(args) as directory:
+        assert not (directory / "PHASER.1.pdb").exists()
