aeb4fa31f7e3cbdd1af9be9a10b1cf9a035f7870
Paul Bond <git@paulsbond.co.uk>
2025-03-25 09:54:37 +0000

    Using demo data for ARP/wARP test

--- test/i2run/test_arpwarp.py
+++ test/i2run/test_arpwarp.py
@@ -1,22 +1,18 @@
-from .urls import pdbe_fasta, redo_mtz
-from .utils import download, i2run
-
 import gemmi
+from .utils import demoData, i2run
 
 
 def test_arpwarp():
     args = ["arp_warp_classic"]
-    with download(redo_mtz("1o6a")) as mtz:
-        with download(pdbe_fasta("1o6a")) as fasta:
-            args += ["--AWA_FOBS", f"fullPath={mtz}", "columnLabels=/*/*/[FP,SIGFP]"]
-            args += ["--AWA_FREE", f"fullPath={mtz}", "columnLabels=/*/*/[FREE]"]
-            args += ["--AWA_PHINI", f"fullPath={mtz}", "columnLabels=/*/*/[PHIC_ALL,FOM]"]
-            args += ["--AWA_PHREF", f"fullPath={mtz}", "columnLabels=/*/*/[PHIC_ALL,FOM]"]
-            args += ["--AWA_SEQIN", f"seqFile={fasta}"]
-            args += ["--AWA_BIG_CYCLES", "1"]
-            args += ["--AWA_SMALL_CYCLES", "1"]
-            with i2run(args) as directory:
-                for name in ["XYZDUM", "XYZOUT"]:
-                    gemmi.read_pdb(str(directory / f"{name}.pdb"))
-                for name in ["DIFFPHIOUT", "FPHIOUT"]:
-                    gemmi.read_mtz_file(str(directory / f"{name}.mtz"))
+    args += ["--AWA_FOBS", demoData("gamma", "merged_intensities_Xe.mtz")]
+    args += ["--AWA_FREE", demoData("gamma", "freeR.mtz")]
+    args += ["--AWA_PHINI", demoData("gamma", "initial_phases.mtz")]
+    args += ["--AWA_PHREF", demoData("gamma", "initial_phases.mtz")]
+    args += ["--AWA_SEQIN", f"seqFile={demoData('gamma', 'gamma.pir')}"]
+    args += ["--AWA_BIG_CYCLES", "1"]
+    args += ["--AWA_SMALL_CYCLES", "1"]
+    with i2run(args) as directory:
+        for name in ["XYZDUM", "XYZOUT"]:
+            gemmi.read_pdb(str(directory / f"{name}.pdb"))
+        for name in ["DIFFPHIOUT", "FPHIOUT"]:
+            gemmi.read_mtz_file(str(directory / f"{name}.mtz"))
