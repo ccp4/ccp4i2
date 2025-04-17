2dde61d5a4c054f7fd3c0218916d000ce36080ea
Paul Bond <git@paulsbond.co.uk>
2025-04-17 15:09:14 +0100

    Aimless test

--- test/i2run/test_aimless.py
+++ test/i2run/test_aimless.py
@@ -1,3 +1,4 @@
+from gemmi import read_mtz_file
 from pytest import mark
 from .utils import demoData, i2run
 
@@ -12,4 +13,10 @@ from .utils import demoData, i2run
 def test_aimless(file: str):
     args = ["aimless_pipe", "--UNMERGEDFILES", f"file={file}"]
     with i2run(args) as job:
-        assert False, "Check output files"
+        for name in [
+            "FREERFLAG",
+            "HKLOUT_0-observed_data_asIMEAN",
+            "HKLOUT_0-observed_data",
+            "HKLOUT_unmerged",
+        ]:
+            read_mtz_file(str(job / f"{name}.mtz"))
