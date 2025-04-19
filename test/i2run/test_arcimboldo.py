32d032fda5f62f4f6be731effaafd9e795f1e554
Paul Bond <git@paulsbond.co.uk>
2025-04-19 14:13:46 +0100

    Arcimboldo test

--- test/i2run/test_arcimboldo.py
+++ test/i2run/test_arcimboldo.py
@@ -1,16 +1,29 @@
 from sys import platform
-from pytest import mark
-from .utils import demoData, i2run
+from gemmi import read_mtz_file, read_pdb
+from pytest import fixture, mark
+from .urls import redo_mtz
+from .utils import download, i2run
 
 
 pytestmark = mark.skipif(platform == "win32", reason="Not supported on Windows")
 
 
-def test_arcimboldo():
+@fixture(scope="module", name="mtz")
+def mtz_fixture():
+    with download(redo_mtz("2ccf")) as path:
+        yield path
+
+
+def test_arcimboldo(mtz):
     args = ["arcimboldo"]
-    args += ["--F_SIGF", demoData("gamma", "merged_intensities_Xe.mtz")]
+    args += ["--F_SIGF", f"fullPath={mtz}", "columnLabels=/*/*/[FP,SIGFP]"]
     args += ["--ARCIMBOLDO_OPTIONS", "LITE"]
-    args += ["--MOLECULAR_WEIGHT", "15000"]
+    args += ["--N_COMPONENTS", "1"]
+    args += ["--MOLECULAR_WEIGHT", "6000"]
     args += ["--N_FRAGMENTS", "1"]
+    args += ["--HELIX_LENGTH", "14"]
     with i2run(args) as job:
-        assert False, "Check output files"
+        read_mtz_file(str(job / "anis.mtz"))
+        read_pdb(str(job / "anis.patterson.pdb"))
+        log = (job / "log.txt").read_text()
+        assert "TNCS was found. Please add another fragment" in log
