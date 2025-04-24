35a9b72155536943322a35133aef8c3a4592e754
Paul Bond <git@paulsbond.co.uk>
2025-04-24 17:14:27 +0100

    Import merged tests

--- test/i2run/test_import_merged.py
+++ test/i2run/test_import_merged.py
@@ -1,8 +1,22 @@
-from .utils import demoData, i2run
+from gemmi import read_mtz_file
+from .urls import pdbe_sfcif
+from .utils import demoData, download, i2run
 
 
-def test_import_merged():
+def test_2ceu_cif():
+    with download(pdbe_sfcif("2ceu")) as cif:
+        args = ["import_merged"]
+        args += ["--HKLIN", str(cif)]
+        args += ["--SPACEGROUP", "I 2 2 2"]
+        with i2run(args) as job:
+            for name in ["OBS", "FREE"]:
+                read_mtz_file(str(job / f"{name}OUT.mtz"))
+            assert False, "Check output files"
+
+
+def test_gamma_mtz():
     args = ["import_merged"]
     args += ["--HKLIN", demoData("gamma", "merged_intensities_Xe.mtz")]
     with i2run(args) as job:
-        assert job.exists()
+        for name in ["OBS", "FREE"]:
+            read_mtz_file(str(job / f"{name}OUT.mtz"))
