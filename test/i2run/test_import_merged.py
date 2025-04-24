8ea765058f85c33f26c7215d653b0a80969a79db
Paul Bond <git@paulsbond.co.uk>
2025-04-24 17:43:08 +0100

    Testing output free-R flags

--- test/i2run/test_import_merged.py
+++ test/i2run/test_import_merged.py
@@ -1,4 +1,4 @@
-from gemmi import read_mtz_file
+import gemmi
 from .urls import pdbe_sfcif
 from .utils import demoData, download, i2run
 
@@ -9,14 +9,40 @@ def test_2ceu_cif():
         args += ["--HKLIN", str(cif)]
         args += ["--SPACEGROUP", "I 2 2 2"]
         with i2run(args) as job:
-            for name in ["OBS", "FREE"]:
-                read_mtz_file(str(job / f"{name}OUT.mtz"))
-            assert False, "Check output files"
+            check_output(job, str(cif))
 
 
 def test_gamma_mtz():
+    ianom = demoData("gamma", "merged_intensities_Xe.mtz")
+    freer = demoData("gamma", "freeR.mtz")
     args = ["import_merged"]
-    args += ["--HKLIN", demoData("gamma", "merged_intensities_Xe.mtz")]
+    args += ["--HKLIN", ianom]
+    args += ["--FREERFLAG", freer]
     with i2run(args) as job:
-        for name in ["OBS", "FREE"]:
-            read_mtz_file(str(job / f"{name}OUT.mtz"))
+        check_output(job, freer)
+
+
+def check_output(job, freerin):
+    gemmi.read_mtz_file(str(job / "OBSOUT.mtz"))
+    input_flags = freer_flag_dict(freerin)
+    output_flags = freer_flag_dict(str(job / "FREEOUT.mtz"))
+    for hkl, free in input_flags.items():
+        assert free == output_flags[hkl], f"Free-R flag mismatch at {hkl}"
+
+
+def freer_flag_dict(hklin):
+    if hklin.endswith(".mtz"):
+        mtz = gemmi.read_mtz_file(hklin)
+    else:
+        doc = gemmi.cif.read(hklin)
+        rblock = gemmi.as_refln_blocks(doc)[0]
+        mtz = gemmi.CifToMtz().convert_block_to_mtz(rblock)
+    mtz.ensure_asu()
+    hcol = mtz.column_with_label("H")
+    kcol = mtz.column_with_label("K")
+    lcol = mtz.column_with_label("L")
+    freecol = mtz.rfree_column()
+    return {
+        (h, k, l): min(free, 1)
+        for h, k, l, free in zip(hcol, kcol, lcol, freecol)
+    }
