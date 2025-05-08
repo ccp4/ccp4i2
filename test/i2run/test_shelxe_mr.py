1add4a525c326f828d19c9ce09c7aa5fa1755b83
Paul Bond <git@paulsbond.co.uk>
2025-05-08 12:54:06 +0100

    SHELXE MR

--- test/i2run/test_shelxe_mr.py
+++ test/i2run/test_shelxe_mr.py
@@ -1,3 +1,4 @@
+import xml.etree.ElementTree as ET
 from gemmi import read_mtz_file, read_pdb
 from .utils import demoData, i2run
 
@@ -15,3 +16,5 @@ def test_gamma():
     with i2run(args) as job:
         read_pdb(str(job / "shelxrun.pdb"))
         read_mtz_file(str(job / "FPHIOUT.mtz"))
+        xml = ET.parse(job / "program.xml")
+        assert float(xml.find(".//BestCC").text) > 44
