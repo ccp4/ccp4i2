ebee43f94d48a82c653a838b4a50cd7b847da8df
Paul Bond <git@paulsbond.co.uk>
2025-05-09 15:46:22 +0100

    Changes for Windows

--- test/i2run/test_find_waters.py
+++ test/i2run/test_find_waters.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from tempfile import NamedTemporaryFile
 import xml.etree.ElementTree as ET
 import gemmi
@@ -10,21 +11,26 @@ def test_8xfm(cif8xfm, mtz8xfm):
         for i, residue in reversed(list(enumerate(chain))):
             if residue.name == "HOH":
                 del chain[i]
-    with NamedTemporaryFile(suffix=".cif") as temp:
-        doc = structure.make_mmcif_document()
-        doc.write_file(temp.name)
-        args = ["coot_find_waters"]
-        args += ["--XYZIN", temp.name]
-        args += ["--FPHIIN", f"fullPath={mtz8xfm}", "columnLabels=/*/*/[FWT,PHWT]"]
+    with NamedTemporaryFile(suffix=".cif", delete=False) as temp:
+        xyzin = temp.name
+    doc = structure.make_mmcif_document()
+    doc.write_file(xyzin)
+    assert not has_water(xyzin)
+    args = ["coot_find_waters"]
+    args += ["--XYZIN", xyzin]
+    args += ["--FPHIIN", f"fullPath={mtz8xfm}", "columnLabels=/*/*/[FWT,PHWT]"]
+    try:
         with i2run(args) as job:
             assert hasLongLigandName(job / "XYZOUT.cif")
-            xml = ET.parse(job / "program.xml")
-            waters_found = int(xml.find("WatersFound").text)
-            assert waters_found > 0
-            waters_in_structure = 0
-            structure = gemmi.read_structure(str(job / "XYZOUT.cif"))
-            for chain in structure[0]:
-                for residue in chain:
-                    if residue.name == "HOH":
-                        waters_in_structure += 1
-            assert waters_in_structure == waters_found
+            assert has_water(job / "XYZOUT.cif")
+    finally:
+        Path(xyzin).unlink(missing_ok=True)
+
+
+def has_water(path):
+    structure = gemmi.read_structure(str(path))
+    for chain in structure[0]:
+        for residue in chain:
+            if residue.name == "HOH":
+                return True
+    return False
