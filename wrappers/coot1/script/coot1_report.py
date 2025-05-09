243cd48831270124398de862762273590848058d
Paul Bond <git@paulsbond.co.uk>
2025-05-09 18:20:44 +0100

    Updated report

--- wrappers/coot1/script/coot1_report.py
+++ wrappers/coot1/script/coot1_report.py
@@ -1,35 +1,11 @@
-from report.CCP4ReportParser import *
-import sys
+from report.CCP4ReportParser import Report
 
 class coot1_report(Report):
-    # Specify which gui task and/or pluginscript this applies to
     TASKNAME = 'coot1'
     RUNNING = False
-    def __init__(self,xmlnode=None,jobInfo={},jobStatus=None,**kw):
-        Report. __init__(self,xmlnode=xmlnode,jobInfo=jobInfo,**kw)
-        self.addText(text='Happily finished')
-        pdbsWrittenPath = './/coot_rebuild/number_output_files'
-        pdbsWrittenStringS = xmlnode.findall(pdbsWrittenPath)
-        if len(pdbsWrittenStringS) > 0:
-            pdbsWrittenString = xmlnode.findall(pdbsWrittenPath)[0].text
-            self.append('<br/>')
-            self.addText(text='Number of PDBs written: ' + pdbsWrittenString)
-        dictsWrittenPath = './/coot_rebuild/number_output_dicts'
-        dictsWrittenStringS = xmlnode.findall(dictsWrittenPath)
-        if len(dictsWrittenStringS) > 0:
-            dictsWrittenString = xmlnode.findall(dictsWrittenPath)[0].text
-            self.append('<br/>')
-            self.addText(text='Number of DICTs written: ' + dictsWrittenString)
-        self.drawWarnings()
+    USEPROGRAMXML = False
 
-    def drawWarnings(self, parent=None):
-        if parent is None: parent = self
-        warnings = self.xmlnode.findall('.//Warnings/Warning')
-        warningsFolder = parent.addFold(label='Warnings', initiallyOpen=True)
-        if len(warnings)>0:
-            for warning in warnings:
-                warningsFolder.addText(text=warning.text,style='color:red;')
-                warningsFolder.append('<br/>')
-        else:
-            warningsFolder.addText(text='No warnings from coot_rebuild')
-        return
+    def __init__(self, xmlnode=None, jobInfo={}, jobStatus=None, **kw):
+        super(). __init__(xmlnode=xmlnode, jobInfo=jobInfo, **kw)
+        self.addDiv(style="clear:both;")
+        self.addText(text='Happily finished')
