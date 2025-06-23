f58d58baa164b05876c153f3573ad0131c2566d7
Paul Bond <git@paulsbond.co.uk>
2025-06-23 14:28:00 -0500

    Removed single-use fixtures

--- test/i2run/conftest.py
+++ test/i2run/conftest.py
@@ -1,12 +1,5 @@
 from pytest import fixture
-from .urls import (
-    pdbe_fasta,
-    rcsb_ligand_cif,
-    rcsb_ligand_sdf,
-    rcsb_mmcif,
-    redo_cif,
-    redo_mtz,
-)
+from .urls import pdbe_fasta, redo_cif, redo_mtz
 from .utils import download
 
 
@@ -25,46 +18,4 @@ def mtz8xfm():
 @fixture(scope="session")
 def seq8xfm():
     with download(pdbe_fasta("8xfm")) as path:
-        yield path
-
-
-@fixture(scope="session")
-def sdfA1LU6():
-    with download(rcsb_ligand_sdf("A1LU6")) as path:
-        yield path
-
-
-@fixture(scope="session")
-def cifA1LU6():
-    with download(rcsb_ligand_cif("A1LU6")) as path:
-        yield path
-
-
-@fixture(scope="session")
-def sdfOEX():
-    with download(rcsb_ligand_sdf("OEX")) as path:
-        yield path
-
-
-@fixture(scope="session")
-def cifOEX():
-    with download(rcsb_ligand_cif("OEX")) as path:
-        yield path
-
-
-@fixture(scope="session")
-def cif4ub6():
-    with download(rcsb_mmcif("4ub6")) as path:
-        yield path
-
-
-@fixture(scope="session")
-def cifAF3():
-    with download(rcsb_ligand_cif("AF3")) as path:
-        yield path
-
-
-@fixture(scope="session")
-def cif4dl8():
-    with download(rcsb_mmcif("4dl8")) as path:
         yield path
--- test/i2run/test_acedrg.py
+++ test/i2run/test_acedrg.py
@@ -1,6 +1,95 @@
+from os import environ
 from pathlib import Path
 import gemmi
-from . import utils
+from .urls import rcsb_ligand_cif, rcsb_ligand_sdf, rcsb_mmcif
+from .utils import download, i2run
+
+
+def test_from_cif_monomer_library():
+    cifPath = Path(environ["CLIBD_MON"], "A", "A1LU6.cif")
+    args = ["LidiaAcedrgNew"]
+    args += ["--MOLSMILESORSKETCH", "DICT"]
+    args += ["--TLC", "A1LU6"]
+    args += ["--DICTIN2", cifPath]
+    args += ["--USE_COORD", "True"]
+    with i2run(args) as job:
+        check_output(job, "A1LU6", hasLongLigandName=True)
+
+
+def test_from_cif_rcsb():
+    with download(rcsb_ligand_cif("A1LU6")) as cifPath:
+        args = ["LidiaAcedrgNew"]
+        args += ["--MOLSMILESORSKETCH", "DICT"]
+        args += ["--TLC", "A1LU6"]
+        args += ["--DICTIN2", cifPath]
+        with i2run(args) as job:
+            check_output(job, "A1LU6", hasLongLigandName=True)
+
+
+def test_from_cif_rcsb_metal_AF3():
+    with download(rcsb_ligand_cif("AF3")) as ligand, download(rcsb_mmcif("4dl8")) as mmcif:
+        args = ["LidiaAcedrgNew"]
+        args += ["--MOLSMILESORSKETCH", "DICT"]
+        args += ["--TLC", "AF3"]
+        args += ["--DICTIN2", ligand]
+        args += ["--TOGGLE_METAL", "True"]
+        args += ["--METAL_STRUCTURE", mmcif]
+        with i2run(args) as job:
+            check_output(job, "AF3")
+
+
+def test_from_mol():
+    molUrl = "https://www.ebi.ac.uk/chebi/saveStructure.do"
+    molUrl += "?defaultImage=true&chebiId=46195&imageId=0"
+    with download(molUrl) as molPath:
+        args = ["LidiaAcedrgNew"]
+        args += ["--MOLSMILESORSKETCH", "MOL"]
+        args += ["--TLC", "PCA"]
+        args += ["--MOLIN", molPath]
+        with i2run(args) as job:
+            check_output(job, "PCA")
+
+
+def test_from_sdf():
+    with download(rcsb_ligand_sdf("A1LU6")) as sdfPath:
+        args = ["LidiaAcedrgNew"]
+        args += ["--MOLSMILESORSKETCH", "MOL"]
+        args += ["--TLC", "A1LU6"]
+        args += ["--MOLIN", sdfPath]
+        with i2run(args) as job:
+            check_output(job, "A1LU6", hasLongLigandName=True)
+
+
+def test_from_smiles():
+    args = ["LidiaAcedrgNew"]
+    args += ["--MOLSMILESORSKETCH", "SMILES"]
+    args += ["--TLC", "HCA"]
+    args += ["--SMILESIN", '"CN1CCC23C4C1CC5=C2C(=C(C=C5)OC)OC3C(=O)CC4"']
+    with i2run(args) as job:
+        check_output(job, "HCA")
+
+
+def test_from_mol2():
+    url = "https://upjv.q4md-forcefieldtools.org/Tutorial/leap-off/Frag-DA.mol2"
+    with download(url) as molPath:
+        args = ["LidiaAcedrgNew"]
+        args += ["--MOLSMILESORSKETCH", "MOL2"]
+        args += ["--TLC", "LIG"]
+        args += ["--MOL2IN", molPath]
+        with i2run(args) as job:
+            check_output(job, "LIG")
+
+
+def test_from_smiles_atom_name_matching():
+    args = ["LidiaAcedrgNew"]
+    args += ["--MOLSMILESORSKETCH", "SMILES"]
+    args += ["--TLC", "LIG"]
+    args += ["--SMILESIN", '"CO[C@@H]1[C@@H]([C@H](O[C@H]1N2C=NC3=C2N=C(NC3=O)N)COP(=O)(O)O)O"']
+    args += ["--ATOMMATCHOPTION", "MONLIBCODE"]
+    args += ["--MATCHTLC", "5GP"]
+    args += ["--NRANDOM", "5"]
+    with i2run(args) as job:
+        check_output(job, "LIG")
 
 
 def check_output(job: Path, code: str, hasLongLigandName=False):
@@ -12,111 +101,34 @@ def check_output(job: Path, code: str, hasLongLigandName=False):
     gemmi.make_chemcomp_from_block(doc[-1])
 
 
-def test_from_mol():
-    molUrl = "https://www.ebi.ac.uk/chebi/saveStructure.do"
-    molUrl += "?defaultImage=true&chebiId=46195&imageId=0"
-    with utils.download(molUrl) as molPath:
-        args = ["LidiaAcedrgNew"]
-        args += ["--MOLSMILESORSKETCH", "MOL"]
-        args += ["--TLC", "PCA"]
-        args += ["--MOLIN", molPath]
-        with utils.i2run(args) as job:
-            check_output(job, "PCA")
+# # These tests crashes as acedrg or metalCoord crash themselves, not an i2 problem.
 
 
-def test_from_smiles():
-    args = ["LidiaAcedrgNew"]
-    args += ["--MOLSMILESORSKETCH", "SMILES"]
-    args += ["--TLC", "HCA"]
-    args += ["--SMILESIN", '"CN1CCC23C4C1CC5=C2C(=C(C=C5)OC)OC3C(=O)CC4"']
-    with utils.i2run(args) as job:
-        check_output(job, "HCA")
+# @fixture(name="cif4ub6", scope="module")
+# def cif4ub6_fixture():
+#     with download(rcsb_mmcif("4ub6")) as path:
+#         yield path
 
 
-def test_from_sdf_rcsb(sdfA1LU6):
-    args = ["LidiaAcedrgNew"]
-    args += ["--MOLSMILESORSKETCH", "MOL"]
-    args += ["--TLC", "A1LU6"]
-    args += ["--MOLIN", sdfA1LU6]
-    with utils.i2run(args) as job:
-        check_output(job, "A1LU6", hasLongLigandName=True)
+# def test_from_cif_rcsb_metal_OEX(cif4ub6):
+#     with download(rcsb_ligand_cif("OEX")) as cifOEX:
+#         args = ["LidiaAcedrgNew"]
+#         args += ["--MOLSMILESORSKETCH", "DICT"]
+#         args += ["--TLC", "OEX"]
+#         args += ["--DICTIN2", cifOEX]
+#         args += ["--TOGGLE_METAL", "True"]
+#         args += ["--METAL_STRUCTURE", cif4ub6]
+#         with i2run(args) as job:
+#             check_output(job, "OEX")
 
 
-def test_from_cif_rcsb(cifA1LU6):
-    args = ["LidiaAcedrgNew"]
-    args += ["--MOLSMILESORSKETCH", "DICT"]
-    args += ["--TLC", "A1LU6"]
-    args += ["--DICTIN2", cifA1LU6]
-    with utils.i2run(args) as job:
-        check_output(job, "A1LU6", hasLongLigandName=True)
-
-
-def test_from_cif_monomer_library():
-    molUrl = "https://raw.githubusercontent.com/MonomerLibrary/"
-    molUrl += "monomers/refs/heads/master/a/A1LU6.cif"
-    with utils.download(molUrl) as molPath:
-        args = ["LidiaAcedrgNew"]
-        args += ["--MOLSMILESORSKETCH", "DICT"]
-        args += ["--TLC", "A1LU6"]
-        args += ["--DICTIN2", molPath]
-        args += ["--USE_COORD", "True"]
-        with utils.i2run(args) as job:
-            check_output(job, "A1LU6", hasLongLigandName=True)
-
-
-def test_from_mol2():
-    molUrl = "https://upjv.q4md-forcefieldtools.org/Tutorial/leap-off/Frag-DA.mol2"
-    with utils.download(molUrl) as molPath:
-        args = ["LidiaAcedrgNew"]
-        args += ["--MOLSMILESORSKETCH", "MOL2"]
-        args += ["--TLC", "LIG"]
-        args += ["--MOL2IN", molPath]
-        with utils.i2run(args) as job:
-            check_output(job, "LIG")
-
-
-def test_from_cif_rcsb_metal_AF3(cifAF3, cif4dl8):
-    args = ["LidiaAcedrgNew"]
-    args += ["--MOLSMILESORSKETCH", "DICT"]
-    args += ["--TLC", "AF3"]
-    args += ["--DICTIN2", cifAF3]
-    args += ["--TOGGLE_METAL", "True"]
-    args += ["--METAL_STRUCTURE", cif4dl8]
-    with utils.i2run(args) as job:
-        check_output(job, "AF3")
-
-
-def test_from_smiles_atom_name_matching():
-    args = ["LidiaAcedrgNew"]
-    args += ["--MOLSMILESORSKETCH", "SMILES"]
-    args += ["--TLC", "LIG"]
-    args += ["--SMILESIN", '"CO[C@@H]1[C@@H]([C@H](O[C@H]1N2C=NC3=C2N=C(NC3=O)N)COP(=O)(O)O)O"']
-    args += ["--ATOMMATCHOPTION", "MONLIBCODE"]
-    args += ["--MATCHTLC", "5GP"]
-    args += ["--NRANDOM", "5"]
-    with utils.i2run(args) as job:
-        check_output(job, "LIG")
-
-
-'''These tests crashes as acedrg or metalCoord crash themselves, not an i2 problem.
-
-def test_from_cif_rcsb_metal_OEX(cifOEX, cif4ub6):
-    args = ["LidiaAcedrgNew"]
-    args += ["--MOLSMILESORSKETCH", "DICT"]
-    args += ["--TLC", "OEX"]
-    args += ["--DICTIN2", cifOEX]
-    args += ["--TOGGLE_METAL", "True"]
-    args += ["--METAL_STRUCTURE", cif4ub6]
-    with utils.i2run(args) as job:
-        check_output(job, "OEX")
-
-def test_from_sdf_rcsb_metal_OEX(sdfOEX, cif4ub6):
-    args = ["LidiaAcedrgNew"]
-    args += ["--MOLSMILESORSKETCH", "MOL"]
-    args += ["--TLC", "OEX"]
-    args += ["--MOLIN", sdfOEX]
-    args += ["--TOGGLE_METAL", "True"]
-    args += ["--METAL_STRUCTURE", cif4ub6]
-    with utils.i2run(args) as job:
-        check_output(job, "OEX")
-'''
+# def test_from_sdf_rcsb_metal_OEX(cif4ub6):
+#     with download(rcsb_ligand_sdf("OEX")) as sdfOEX:
+#         args = ["LidiaAcedrgNew"]
+#         args += ["--MOLSMILESORSKETCH", "MOL"]
+#         args += ["--TLC", "OEX"]
+#         args += ["--MOLIN", sdfOEX]
+#         args += ["--TOGGLE_METAL", "True"]
+#         args += ["--METAL_STRUCTURE", cif4ub6]
+#         with i2run(args) as job:
+#             check_output(job, "OEX")
