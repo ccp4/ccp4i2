6ea304d5c712de64a23456c3cc9b37414b8aece1
Paul Bond <git@paulsbond.co.uk>
2025-06-23 20:09:27 -0500

    Spacing

--- wrappers/modelASUCheck/script/modelASUCheck_gui.py
+++ wrappers/modelASUCheck/script/modelASUCheck_gui.py
@@ -22,7 +22,7 @@ from qtgui.CCP4TaskWidget import CTaskWidget
 class modelASUCheck_gui(CTaskWidget):
     TASKNAME = 'modelASUCheck'
     TASKVERSION = 0.1
-    TASKMODULE = [ 'model_data_utility' ]
+    TASKMODULE = ['model_data_utility']
     SHORTTASKTITLE = 'Check model against AU contents'
     TASKTITLE = 'Check model against AU contents'
     DESCRIPTION = 'Align sequences in model to those in AU contents to validate model.'
@@ -30,13 +30,13 @@ class modelASUCheck_gui(CTaskWidget):
     def drawContents(self):
         self.openFolder(folderFunction='inputData',followFrom=False)
 
-        self.createLine(['subtitle','Input coordinates'])
+        self.createLine(['subtitle', 'Input coordinates'])
         self.openSubFrame(frame=True)
-        self.createLine ( [ 'tip','Input model','widget','XYZIN' ] )
+        self.createLine (['tip', 'Input model', 'widget', 'XYZIN'])
         self.getWidget('XYZIN').showAtomSelection()
         self.closeSubFrame()
 
-        self.createLine(['subtitle','Input AU Contents'])
+        self.createLine(['subtitle', 'Input AU Contents'])
         self.openSubFrame(frame=True)
-        self.createLine ( [ 'tip','Input AU Contents','widget','ASUIN' ] )
+        self.createLine (['tip', 'Input AU Contents', 'widget', 'ASUIN'])
         self.closeSubFrame()
