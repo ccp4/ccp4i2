a01898edd8afbf7542b15bd00b3dfaf62e2c818e
Jon Agirre <jon.agirre@york.ac.uk>
2025-07-15 15:03:11 +0100

    Iris bug fixes, new icon, ability to run with just one model (#82)

--- wrappers/validate_protein/script/validate_protein_report.py
+++ wrappers/validate_protein/script/validate_protein_report.py
@@ -179,9 +179,9 @@ class validate_protein_report(Report):
         section_div = parent.addDiv()
         section_div.append('<p style="font-weight:bold; font-size:14px;"><b>Ramachandran Analyses</b></p>')
 
-        background_PRO = 'http://127.0.0.1:43434/report_files/0.1.0/rama_pro.png'
-        background_GLY = 'http://127.0.0.1:43434/report_files/0.1.0/rama_gly.png'
-        background_RST = 'http://127.0.0.1:43434/report_files/0.1.0/rama_rst.png'
+        background_PRO = htmlBase() + '/rama_pro.png'
+        background_GLY = htmlBase() + '/rama_gly.png'
+        background_RST = htmlBase() + '/rama_rst.png'
 
         graph_div = parent.addDiv(style='float:left; margin-left:50px;')
         rama_graph = graph_div.addFlotGraph(title='Ramachandran Plot', select='.//Ramachandran', style='height:500px; width:500px; border:0px; float:left; padding:10px; padding-left:15px;')
@@ -255,10 +255,13 @@ class validate_protein_report(Report):
             table_outliers.addData(title='Name',  data=[ x.attrib["type"] for x in self.xmlnode.findall('.//Ramachandran/Outliers/Residue') if "type" in x.attrib])
             table_outliers.addData(title='Residue',    data=[ x.attrib["seqnum"] for x in self.xmlnode.findall('.//Ramachandran/Outliers/Residue') if "seqnum" in x.attrib])
 
-        text ='<b>%s</b> residues have been analysed.<br/><br/>' % n_residues
-        text +='In <b><font color=\'green\'>favoured</font></b> regions: <b>%s (%0.2f%%)</b><br/>' % (n_favoured, 100 * n_favoured / n_residues)
-        text +='In <b><font color=\'orange\'>allowed</font></b> regions: <b>%s (%0.2f%%)</b><br/>' % (n_allowed, 100 * n_allowed / n_residues)
-        text +='In <b><font color=\'red\'>high-energy</font></b> backbone conformations (outliers): <b>%s (%0.2f%%)</b>' % (n_outliers, 100 * n_outliers / n_residues)
+        if n_residues == 0:
+            text = 'No residues have been analysed.<br/><br/>'
+        else :
+            text ='<b>%s</b> residues have been analysed.<br/><br/>' % n_residues
+            text +='In <b><font color=\'green\'>favoured</font></b> regions: <b>%s (%0.2f%%)</b><br/>' % (n_favoured, 100 * n_favoured / n_residues)
+            text +='In <b><font color=\'orange\'>allowed</font></b> regions: <b>%s (%0.2f%%)</b><br/>' % (n_allowed, 100 * n_allowed / n_residues)
+            text +='In <b><font color=\'red\'>high-energy</font></b> backbone conformations (outliers): <b>%s (%0.2f%%)</b>' % (n_outliers, 100 * n_outliers / n_residues)
         tab_div.append(text)
 
         section_div.addDiv(style='clear:both;')
