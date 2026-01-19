8597916401b31e7af3f800830807592b36220107
Paul Bond <git@paulsbond.co.uk>
2026-01-19 17:01:27 +0000

    Zanuda updates

--- server/ccp4i2/wrappers/zanuda/script/zanuda.py
+++ server/ccp4i2/wrappers/zanuda/script/zanuda.py
@@ -2,82 +2,49 @@ import os
 import re
 
 from ccp4i2.core.CCP4ErrorHandling import SEVERITY_WARNING
-from ccp4i2.core.CCP4ModelData import CPdbDataFile
 from ccp4i2.core.CCP4PluginScript import CPluginScript
-from ccp4i2.core.CCP4XtalData import CMapCoeffsDataFile, CObsDataFile, CPhsDataFile
+from ccp4i2.core.CCP4XtalData import CObsDataFile
 
 
 class zanuda(CPluginScript):
     TASKMODULE = "refinement"
     TASKTITLE = "Zanuda"
     TASKNAME = "zanuda"
-    TASKVERSION = 0.1
     TASKCOMMAND = "zanuda"
     MAINTAINER = "andrey.lebedev@stfc.ac.uk"
-    ERROR_CODES = {
-        201: {"description": "Failed to analyse output files"},
-        202: {"description": "Failed applying selection to PDB file"},
-    }
-    PURGESEARCHLIST = [["hklin.mtz", 0], ["log_mtzjoin.txt", 0]]
     PERFORMANCECLASS = "CRefinementPerformance"
 
-    def __init__(self, *args, **kws):
-        super(zanuda, self).__init__(*args, **kws)
-
     def processInputFiles(self):
-        miniMtzs = [
-            ["F_SIGF", CObsDataFile.CONTENT_FLAG_FMEAN],
-            ["FREERFLAG", None],
-        ]
-        self.hklin, self.columns, error = self.makeHklin0(miniMtzs)
-        if error.maxSeverity() > SEVERITY_WARNING:
-            return CPluginScript.FAILED
-        self.model = os.path.join(self.getWorkDirectory(), "model.xyz")
-        self.container.inputData.XYZIN.getSelectedAtomsPdbFile(self.model)
-        return CPluginScript.SUCCEEDED
+        self.xyzin = os.path.join(self.getWorkDirectory(), "model.xyz")
+        self.container.inputData.XYZIN.getSelectedAtomsPdbFile(self.xyzin)
+        miniMtzs = [["F_SIGF", CObsDataFile.CONTENT_FLAG_FMEAN], "FREERFLAG"]
+        self.hklin, _, errorReport = self.makeHklin0(miniMtzs)
+        return errorReport
 
     def makeCommandAndScript(self):
-        self.appendCommandLine(["xyzin", self.model])
+        self.appendCommandLine(["xyzin", self.xyzin])
         self.appendCommandLine(["hklin", self.hklin])
         self.appendCommandLine(["xyzout", "zanuda.pdb"])
         self.appendCommandLine(["hklout", "zanuda.mtz"])
-        params = self.container.controlParameters
-        if params.AVERAGE:
-          self.appendCommandLine(["aver"])
-        tmpdir = os.path.join(self.getWorkDirectory(), "tmpdir")
-        self.appendCommandLine(["tmpdir", tmpdir])
-        # for tests (faster):
-        # self.appendCommandLine(["notwin"])
+        if self.container.controlParameters.AVERAGE:
+            self.appendCommandLine(["aver"])
+        self.appendCommandLine(["tmpdir", self.workDirectory / "tmpdir"])
 
     def processOutputFiles(self):
-        directory = self.getWorkDirectory()
-        zanuda_pdb = os.path.join(directory, "zanuda.pdb")
-        zanuda_mtz = os.path.join(directory, "zanuda.mtz")
-        outputData = self.container.outputData
-        outputData.XYZOUT.setFullPath(zanuda_pdb)
-        outputData.XYZOUT.annotation.set("Zanuda model")
-        outputData.XYZOUT.subType.set(CPdbDataFile.SUBTYPE_MODEL)
-        outputData.XYZOUT.contentFlag.set(CPdbDataFile.CONTENT_FLAG_PDB)
-        outputData.FPHIOUT.annotation.set("Zanuda best map")
-        outputData.FPHIOUT.subType.set(CMapCoeffsDataFile.SUBTYPE_NORMAL)
-        outputData.FPHIOUT.contentFlag.set(CMapCoeffsDataFile.CONTENT_FLAG_FPHI)
-        outputData.DIFFPHIOUT.annotation.set("Zanuda difference map")
-        outputData.DIFFPHIOUT.subType.set(CMapCoeffsDataFile.SUBTYPE_DIFFERENCE)
-        outputData.DIFFPHIOUT.contentFlag.set(CMapCoeffsDataFile.CONTENT_FLAG_FPHI)
+        out = self.container.outputData
+        out.XYZOUT.fullPath = self.workDirectory / "zanuda.pdb"
         files = ["FPHIOUT", "DIFFPHIOUT"]
         columns = ["FWT,PHWT", "DELFWT,PHDELWT"]
-        error = self.splitHklout(files, columns, zanuda_mtz)
+        error = self.splitHklout(files, columns, self.workDirectory / "zanuda.mtz")
         if error.maxSeverity() > SEVERITY_WARNING:
-            return CPluginScript.FAILED
-        log = os.path.join(self.getWorkDirectory(), 'log.txt')
-        if os.path.isfile(log):
-            p2 = '^ *[|] *([<> ]{2}) *([0-9]+) *[|]'
-            p2 += ' *([A-Z][0-9 ]*[0-9]) *[|]'
-            p2 += 4* ' *(--|[0-9.]+|error) *[|]' + ' *$'
-            with open(log) as istream:
+            return error
+        log = self.workDirectory / "log.txt"
+        if log.is_file():
+            p2 = "^ *[|] *([<> ]{2}) *([0-9]+) *[|]"
+            p2 += " *([A-Z][0-9 ]*[0-9]) *[|]"
+            p2 += 4 * " *(--|[0-9.]+|error) *[|]" + " *$"
+            with log.open(encoding="utf-8") as istream:
                 rr = re.findall(p2, istream.read(), re.M)
-            if rr and rr[-1][0] == '<<':
-                outputData.PERFORMANCE.RFactor.set(rr[-1][-2])
-                outputData.PERFORMANCE.RFree.set(rr[-1][-1])
-        return CPluginScript.SUCCEEDED
-
+            if rr and rr[-1][0] == "<<":
+                out.PERFORMANCE.RFactor = rr[-1][-2]
+                out.PERFORMANCE.RFree = rr[-1][-1]
