eceef66c7a7df7da936e707c1707339fa8bbf605
Paul Bond <git@paulsbond.co.uk>
2026-01-28 16:33:39 +0000

    Only printing if running as main

--- server/ccp4i2/pipelines/crank2/crank2/crank2.py
+++ server/ccp4i2/pipelines/crank2/crank2/crank2.py
@@ -9,11 +9,10 @@ from . import parse,common
 #  sys.exit('Error: Environment variable CRANK2 points to non-existing directory {0}'.format(
 #    os.getenv('CRANK2')))
 
-version_file=os.path.join(os.path.dirname(parse.__file__), 'VERSION')
-if not os.path.isfile(version_file):
-  common.Error('VERSION file could not be found in {0}'.format(os.path.dirname(parse.__file__)))
-with open(version_file) as f:
-  common.Info('CRANK2, version {0}'.format(f.readline()))
-
 if __name__ == "__main__":
+  version_file=os.path.join(os.path.dirname(parse.__file__), 'VERSION')
+  if not os.path.isfile(version_file):
+    common.Error('VERSION file could not be found in {0}'.format(os.path.dirname(parse.__file__)))
+  with open(version_file) as f:
+    common.Info('CRANK2, version {0}'.format(f.readline()))
   parse.parse().ParseAndRun()
