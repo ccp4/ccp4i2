ed26c901a2a79a1e4a1cb65e82e6b87d662fdd1e
Paul Bond <git@paulsbond.co.uk>
2026-02-20 15:37:52 +0000

    Empty cli.tools tools package

--- server/ccp4i2/cli/tools/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-CCP4i2 CLI Tools
-
-Utility scripts for CCP4i2 operations:
-- buildCaches: Build task definition caches
-- makeReport: Generate reports from job outputs
-- runTask: Run individual tasks
-"""
